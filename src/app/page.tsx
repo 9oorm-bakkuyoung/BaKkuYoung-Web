648f6dd2671f218771885eb922c1fde4ca3eea5e
seunghoonKang <tmdgns4321@gmail.com>
2025-02-21 06:14:46 +0900

    [Feat] main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,45 @@
+"use client";
+
+import PigeonAnimation from "@/components/PigeonAnimation.js";
+
 export default function Home() {
-    return (
-        <main className="min-h-screen bg-gradient-to-br from-emerald-400 to-cyan-300 p-6 flex flex-col items-center">
-            {/* 메인 소개 (상단 고정) */}
-            <div className="w-full text-center mt-20">
-                <h1 className="text-white text-2xl font-bold">필요한 순간, 필요한 물건</h1>
-            </div>
+  return (
+    <main className="relative flex min-h-screen flex-col items-center bg-gradient-to-br from-emerald-400 to-cyan-300 p-6">
+      {/* 메인 소개 (상단 고정) */}
 
-            {/* SVG 이미지 업로드 예 */}
-            <div className="flex flex-col items-center space-y-4">
-                {/* 1. 직접 코드 삽입 방법 */}
-                {/*<div className="w-16 h-16">*/}
-                {/*    <svg*/}
-                {/*        xmlns="http://www.w3.org/2000/svg"*/}
-                {/*        fill="none"*/}
-                {/*        viewBox="0 0 24 24"*/}
-                {/*        stroke="currentColor"*/}
-                {/*        className="w-full h-full text-white"*/}
-                {/*    >*/}
-                {/*        <path*/}
-                {/*            strokeLinecap="round"*/}
-                {/*            strokeLinejoin="round"*/}
-                {/*            strokeWidth="2"*/}
-                {/*            d="M12 4v16m8-8H4"*/}
-                {/*        />*/}
-                {/*    </svg>*/}
-                {/*</div>*/}
+      <div>
+        <div className="flex flex-col gap-[15px]">
+          <div className="mt-[59px] w-full text-center">
+            <h1 className="text-2xl font-bold text-white">
+              필요한 순간, 필요한 물건
+            </h1>
+          </div>
+          <img src="/icons/bakkuyoung.svg" alt="bakkuyoung" className="z-10" />
+        </div>
 
-                {/* 2. public 폴더에서 불러오기 */}
-                <img src="/icons/bakkuyoung.svg" alt="샘플 아이콘" className="w-56 h-44" />
+        <img
+          src="/arrow_bakkuyoung.svg"
+          className="absolute top-[-82px] left-0 h-[852px] w-full"
+          //   className="absolute top-[-82px] left-0 w-full"
+        />
 
-                {/* 3. React 컴포넌트로 불러오기 (Next.js 환경) */}
-                {/* 예: components/SampleIcon.tsx */}
-                {/* <SampleIcon className="w-16 h-16 text-white" /> */}
-            </div>
-        </main>
-    );
+        <img
+          src="/hello_jeju.svg"
+          className="absolute top-[400px] left-[30px] z-10"
+        />
+
+        <PigeonAnimation />
+
+        {/* <img
+          src="/hello_pigeon_bg.svg"
+          className="absolute right-0 bottom-0 h-[500px] w-[500px]"
+        />
+
+        <img
+          src="/hello_pigeon.svg"
+          className="absolute right-0 bottom-0 h-[520px] w-[350px]"
+        /> */}
+      </div>
+    </main>
+  );
 }
