3e655172d47709c7396c09075aad5d9211bfa7db
rhctmxk <rhctmxk@naver.com>
2025-02-20 11:26:09 +0900

    [Feat] 기본 해상도 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     return (
         <>
           <button className="bg-gray-800 px-4 py-2 rounded-md text-white">Button</button>
+          프리텐더
         </>
     );
 }
