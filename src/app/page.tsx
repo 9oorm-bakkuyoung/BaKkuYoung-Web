bcd99866a7ee303b16a780de295613469621c5da
seunghoonKang <tmdgns4321@gmail.com>
2025-02-21 06:19:48 +0900

    [Fix] main error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import PigeonAnimation from "@/components/PigeonAnimation.js";
-
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center bg-gradient-to-br from-emerald-400 to-cyan-300 p-6">
-      {/* 메인 소개 (상단 고정) */}
-
       <div>
         <div className="flex flex-col gap-[15px]">
           <div className="mt-[59px] w-full text-center">
@@ -20,7 +16,6 @@ export default function Home() {
         <img
           src="/arrow_bakkuyoung.svg"
           className="absolute top-[-82px] left-0 h-[852px] w-full"
-          //   className="absolute top-[-82px] left-0 w-full"
         />
 
         <img
@@ -28,9 +23,7 @@ export default function Home() {
           className="absolute top-[400px] left-[30px] z-10"
         />
 
-        <PigeonAnimation />
-
-        {/* <img
+        <img
           src="/hello_pigeon_bg.svg"
           className="absolute right-0 bottom-0 h-[500px] w-[500px]"
         />
@@ -38,7 +31,7 @@ export default function Home() {
         <img
           src="/hello_pigeon.svg"
           className="absolute right-0 bottom-0 h-[520px] w-[350px]"
-        /> */}
+        />
       </div>
     </main>
   );
